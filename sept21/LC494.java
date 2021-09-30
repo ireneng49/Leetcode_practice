821a156cf3f1dca5146e219d6dd06edbaee00006
Irene Ng <30131788+ireneng49@users.noreply.github.com>
2021-09-30 15:25:04 -0700

    add LC494 & 876

---
+++ sept21/LC494.java
@@ -0,0 +1,44 @@
+/**
+ * 494. Target Sum
+ * You are given an integer array nums and an integer target.
+
+You want to build an expression out of nums by adding one of the symbols '+' and '-' before each integer in nums and then concatenate all the integers.
+
+For example, if nums = [2, 1], you can add a '+' before 2 and a '-' before 1 and concatenate them to build the expression "+2-1".
+Return the number of different expressions that you can build, which evaluates to target.
+
+Example 1:
+
+Input: nums = [1,1,1,1,1], target = 3
+Output: 5
+Explanation: There are 5 ways to assign symbols to make the sum of nums be target 3.
+-1 + 1 + 1 + 1 + 1 = 3
++1 - 1 + 1 + 1 + 1 = 3
++1 + 1 - 1 + 1 + 1 = 3
++1 + 1 + 1 - 1 + 1 = 3
++1 + 1 + 1 + 1 - 1 = 3
+Example 2:
+
+Input: nums = [1], target = 1
+Output: 1
+ */
+
+class Solution {
+    int count = 0;
+   
+   public int findTargetSumWays(int[] nums, int S) {
+       calculate(nums, 0, 0, S);
+       return count;
+   }
+   
+   public void calculate(int[] nums, int i, int sum, int S) {
+       if (i == nums.length) {
+           if (sum == S) {
+               count++;
+           }
+       } else {
+           calculate(nums, i + 1, sum + nums[i], S);
+           calculate(nums, i + 1, sum - nums[i], S);
+       }
+   }
+}
