f0b4a7fb25d00ff9b8adc84b50697d5404cfc441
Irene Ng <30131788+ireneng49@users.noreply.github.com>
2021-11-01 22:32:48 -0700

    add nov

---
+++ nov21/validate_subsequence.java
@@ -0,0 +1,20 @@
+/**
+ * Validate Subsequence
+ */
+
+lass Program {
+    public static boolean isValidSubsequence(List<Integer> array, List<Integer> sequence) {
+      // Write your code here.
+          int arrIdx = 0;
+          int seqIdx = 0;
+          while (arrIdx < array.size() && seqIdx < sequence.size()) {
+              if (array.get(arrIdx).equals(sequence.get(seqIdx))) {
+                  seqIdx++;
+              }
+              arrIdx++;
+          }
+          
+      return seqIdx == sequence.size();
+    }
+  }
+  
