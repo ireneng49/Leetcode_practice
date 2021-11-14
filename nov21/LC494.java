8a557b3eed345747fdb5b44207bb7318f1d79040
Irene Ng <30131788+ireneng49@users.noreply.github.com>
2021-11-13 23:46:36 -0800

    LC494

---
+++ nov21/LC494.java
@@ -0,0 +1,20 @@
+class Solution {
+    public static int findTargetSumWays(int[] nums, int S) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if (sum < S || (sum + S) % 2 == 1) {
+            return 0;
+        }
+        int w = (sum + S) / 2;
+        int[] dp = new int[w + 1];
+        dp[0] = 1;
+        for (int num : nums) {
+            for (int j = w; j >= num; j--) {
+                dp[j] += dp[j - num];
+            }
+        }
+        return dp[w];
+    }
+}
